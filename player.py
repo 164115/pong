b7963e9ec3810cac1d1b7aed967e4373ac73bf4d
James Pirie <131404@cloud.rangitoto.school.nz>
2021-03-10 11:54:28 +1300

    Attempting to format the player code with classes, the player is currently imobile, need to fix this.

--- player.py
+++ player.py
@@ -1,51 +1,50 @@
 from tkinter import *
-
 window = Tk()
 canvas = Canvas(window, width="1920", height="1080", background="black")
 canvas.pack()
 
-can_move_down = True
-can_move_up = True
+
+class player:
+    def __init__(self, c, w):
+        self.player_x_val = 0
+        self.player_y_vel = 0
+        self.can_move_down = True
+        self.can_move_up = True
+        self.player_object = canvas.create_rectangle(20, 100, 100, 400, fill="white")
+        self.canvas = c
+        self.window = w
+
+    def key_released(self, event):
+        if event.keysym == "Up" or event.keysym == "Down":
+            self.player_y_vel = 0
+
+    def key_pressed(self, event):
+        if event.keysym == "Up" and self.can_move_down:
+            self.player_y_vel = -7
+        elif event.keysym == "Down" and self.can_move_up:
+            self.player_y_vel = 7
+
+    def move_stuff(self):
+        self.canvas.move(player, self.player_x_val, self.player_y_vel)
+        self.window.after(16, self.move_stuff)
+        self.can_move_down = True
+        self.can_move_up = True
+        if self.canvas.coords(self.player_object)[1] < 0:  # Top wall
+            self.player_y_vel = 0
+            self.can_move_down = False
+        elif self.canvas.coords(self.player_object)[3] > 920:  # Bottom wall
+            self.player_y_vel = 0
+            self.can_move_up = False
+
+    def initialise_player(self):
+        self.canvas.bind("<KeyPress>", self.key_pressed)
+        self.canvas.bind("<KeyRelease>", self.key_released)
+        self.canvas.focus_set()
+        self.move_stuff()
 
 
-def key_released(event):
-    global player_x_val, player_y_vel
-    if event.keysym == "Up" or event.keysym == "Down":
-        player_y_vel = 0
+player_1 = player(canvas, window)
+player_1.initialise_player()
 
-
-def key_pressed(event):
-    global player_x_val, player_y_vel, can_move_down, can_move_up
-    if event.keysym == "Up" and can_move_down:
-        player_y_vel = -7
-    elif event.keysym == "Down" and can_move_up:
-        player_y_vel = 7
-
-
-def move_stuff():
-    global player_y_vel, player_x_val, can_move_down, can_move_up
-    canvas.move(player, player_x_val, player_y_vel)
-    window.after(16, move_stuff)
-    can_move_down = True
-    can_move_up = True
-    if canvas.coords(player)[1] < 0:  # Top wall
-        player_y_vel = 0
-        can_move_down = False
-    elif canvas.coords(player)[3] > 920:  # Right wall
-        player_y_vel = 0
-        can_move_up = False
-
-
-
-
-player_x_val = 0
-player_y_vel = 0
-
-canvas.bind("<KeyPress>", key_pressed)
-canvas.bind("<KeyRelease>", key_released)
-canvas.focus_set()
-player = canvas.create_rectangle(20, 100, 100, 400, fill="white")
-
-move_stuff()
 
 window.mainloop()
