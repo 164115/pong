0d1260a03bfa53a7f2e9df6708c76723fe9b0e40
James Pirie <131404@cloud.rangitoto.school.nz>
2021-03-10 10:34:57 +1300

    The player can no longer move past the window

--- player.py
+++ player.py
@@ -1,9 +1,11 @@
 from tkinter import *
-import random
 
 window = Tk()
 canvas = Canvas(window, width="1920", height="1080", background="blanchedalmond")
 canvas.pack()
+
+can_move_down = True
+can_move_up = True
 
 
 def key_released(event):
@@ -13,29 +15,27 @@ def key_released(event):
 
 
 def key_pressed(event):
-    global player_x_val, player_y_vel
-    print("event", event)
-    if event.keysym == "Up":
+    global player_x_val, player_y_vel, can_move_down, can_move_up
+    if event.keysym == "Up" and can_move_down:
         player_y_vel = -7
-    if event.keysym == "Down":
+    elif event.keysym == "Down" and can_move_up:
         player_y_vel = 7
 
 
 def move_stuff():
-    canvas.move(walter, player_x_val, player_y_vel)
+    global player_y_vel, player_x_val, can_move_down, can_move_up
+    canvas.move(player, player_x_val, player_y_vel)
     window.after(16, move_stuff)
+    can_move_down = True
+    can_move_up = True
+    if canvas.coords(player)[1] < 0:  # Top wall
+        player_y_vel = 0
+        can_move_down = False
+    elif canvas.coords(player)[3] > 920:  # Right wall
+        player_y_vel = 0
+        can_move_up = False
 
-"""
-def barrier(event):
-    global player_x_val, player_y_vel
-    if canvas.coords(walter)[2] == 1920:  # Bottom wall
-        if event.keysym == "Down":
-            player_y_vel = 7
 
-    if canvas.coords(walter)[1] == 0:  # Top wall
-        if event.keysym == "Up":
-            player_y_vel = -14
-"""
 
 
 player_x_val = 0
@@ -44,7 +44,7 @@ player_y_vel = 0
 canvas.bind("<KeyPress>", key_pressed)
 canvas.bind("<KeyRelease>", key_released)
 canvas.focus_set()
-walter = canvas.create_rectangle(20, 100, 100, 400, fill="white")
+player = canvas.create_rectangle(20, 100, 100, 400, fill="white")
 
 move_stuff()
 
